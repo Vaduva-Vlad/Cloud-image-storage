6edf80ce2155835446e02ae92dc21c67c9d185d6
HVTom <hritu.tomavlad@gmail.com>
2024-06-09 14:32:13 +0300

    visual additions + image extension

--- storage_service/app/routes.py
+++ storage_service/app/routes.py
@@ -15,3 +15,21 @@ def images():
     blob = bucket.blob(image.filename)
     blob.upload_from_file(image, content_type=image.content_type)
     return "OK"
+
+
+# incearca sa scrii functia asa
+# ca sa poti prelua numele imaginii
+"""
+@app.route("/images", methods=["POST"])
+def images():
+    image = request.files.get('imagefile')
+    if image:
+        filename = image.filename #this
+        client = storage.Client()
+        bucket = client.get_bucket('bd_backup_imagini')
+        blob = bucket.blob(filename)
+        blob.upload_from_file(image, content_type=image.content_type)
+        return "OK"
+    else:
+        return "No file uploaded", 400
+"""
